14cb2bbe0ff367ee37e32a7f0f999379d7e8bae9
Andrewboy159 <andrewelliott159@gmail.com>
2017-09-13 00:29:13 -0400

    Added Main Menu Basis

--- src/tk/andrewboy159/pastebinclient/Main.java
+++ src/tk/andrewboy159/pastebinclient/Main.java
@@ -1,15 +1,24 @@
 package tk.andrewboy159.pastebinclient;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    Stage window;
-    Scene splash, main;
+    private Stage window;
+    private Scene splash, main;
+
+    private String key = "e327e1850a5fab998d4a8043d1898c64";
+
+    public String getKey() {
+        return key;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -18,11 +27,13 @@ public class Main extends Application {
         Parent splashLayout = FXMLLoader.load(getClass().getResource("/fxml/splash.fxml"));
         window.setTitle("Pastebin");
         window.setResizable(false);
+        window.getIcons().add(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        window.initStyle(StageStyle.UNDECORATED);
         splash = new Scene(splashLayout, 600, 550);
         window.setScene(splash);
         window.show();
 
-        wait(3);
+        wait(10);
 
 
         Parent mainLayout = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
@@ -30,12 +41,10 @@ public class Main extends Application {
         window.setResizable(false);
         main = new Scene(mainLayout, 600, 550);
         window.setScene(main);
+    }
 
-        //Parent root = FXMLLoader.load(getClass().getResource("/fxml/splash.fxml"));
-        //primaryStage.setTitle("Pastebin");
-        //primaryStage.setScene(new Scene(root, 600, 550));
-        //primaryStage.setResizable(false);
-        //primaryStage.show();
+    public void closeProgram() {
+        Platform.exit();
     }
 
 
--- src/tk/andrewboy159/pastebinclient/controller/ControllerMain.java
+++ src/tk/andrewboy159/pastebinclient/controller/ControllerMain.java
@@ -1,4 +1,105 @@
 package tk.andrewboy159.pastebinclient.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import tk.andrewboy159.pastebinclient.Main;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ControllerMain {
+
+    Main main = new Main();
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private String username;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private String password;
+    private Stage window;
+
+    @FXML
+    private void close() {
+        main.closeProgram();
+    }
+
+    @FXML
+    private void loginWindow() {
+        Parent loginLayout;
+        try {
+            loginLayout = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+            window = new Stage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Log In");
+        window.setResizable(false);
+        window.initStyle(StageStyle.UNDECORATED);
+
+        Scene login = new Scene(loginLayout);
+        window.setScene(login);
+        window.showAndWait();
+    }
+
+    @FXML
+    public String login() {
+        username = usernameField.getText();
+        password = passwordField.getText();
+
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL("http://pastebinclient.tk/server/login.php");
+            String urlParameters = "api_dev_key=" + main.getKey() + "&api_user_name=" + username + "&api_user_password=" + password;
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");
+
+            connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuffer response = new StringBuffer(); // or StringBuffer if Java version 5+
+            String line;
+            while((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\n');
+            }
+            rd.close();
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if(connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
 }
